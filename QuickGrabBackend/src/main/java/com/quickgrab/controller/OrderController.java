45da4c5753403ff5726f2c6178294d693068b464
aritra.lahiri <aritra.lahiri@host.docker.internal>
2022-09-15 09:37:14 +0530

    add order rest endpoints

--- QuickGrabBackend/src/main/java/com/quickgrab/controller/OrderController.java
+++ QuickGrabBackend/src/main/java/com/quickgrab/controller/OrderController.java
@@ -1,14 +1,18 @@
 package com.quickgrab.controller;
 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.quickgrab.exception.ResourceNotFoundException;
 
 import com.quickgrab.model.OrderModel;
 import com.quickgrab.service.OrderService;
@@ -31,6 +35,19 @@ public class OrderController {
 	public ResponseEntity<OrderModel> addFoodToOrder(@PathVariable Integer fId, @PathVariable Integer oId) {
 
 		return orderService.addFoodToOrder(fId, oId);
+
+	}
+
+	@GetMapping
+	public ResponseEntity<List<OrderModel>> showOrders() {
+		return orderService.showAllOrders();
+	}
+
+	@DeleteMapping("{oid}")
+
+	public OrderModel deleteOrder(@PathVariable Integer oid) throws ResourceNotFoundException {
+
+		return orderService.deleteOrder(oid);
 
 	}
 
--- QuickGrabBackend/src/main/java/com/quickgrab/service/OrderService.java
+++ QuickGrabBackend/src/main/java/com/quickgrab/service/OrderService.java
@@ -1,10 +1,13 @@
 package com.quickgrab.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.quickgrab.exception.ResourceNotFoundException;
 import com.quickgrab.model.FoodModel;
 import com.quickgrab.model.OrderModel;
 import com.quickgrab.repository.FoodRepo;
@@ -25,13 +28,34 @@ public class OrderService {
 		return new ResponseEntity<OrderModel>(orderRepo.save(orderModel), HttpStatus.ACCEPTED);
 	}
 
-	public ResponseEntity<OrderModel> addFoodToOrder(Integer orderId, Integer foodId) {
+	public ResponseEntity<OrderModel> addFoodToOrder(Integer foodId, Integer orderId) {
 
 		OrderModel orderModel = orderRepo.findById(orderId).orElse(null);
 		FoodModel foodModel = foodRepo.findById(foodId).orElse(null);
-		
-//		orderModel.getFoodModel().add(foodModel);
+		orderModel.getFoodModel().add(foodModel);
 		return new ResponseEntity<OrderModel>(orderRepo.save(orderModel), HttpStatus.ACCEPTED);
+	}
+
+	public ResponseEntity<List<OrderModel>> showAllOrders() {
+
+		List<OrderModel> orderModel = orderRepo.findAll();
+
+		return new ResponseEntity<List<OrderModel>>(orderModel, HttpStatus.ACCEPTED);
+	}
+
+	public OrderModel deleteOrder(Integer orderId) throws ResourceNotFoundException {
+
+		if (!orderRepo.existsById(orderId)) {
+
+			throw new ResourceNotFoundException("Order Id not found");
+
+		} else {
+
+			orderRepo.deleteById(orderId);
+
+		}
+
+		return null;
 	}
 
 }
